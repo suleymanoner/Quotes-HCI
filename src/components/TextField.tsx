e8db56caa75365ac9fb683eb633a3b013b331d8a
Suleyman Oner <suleymanoner1999@gmail.com>
2022-06-09 15:17:17 +0200

    user card added, components editted

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,48 +1,52 @@
 import React from 'react';
-import { View, StyleSheet, TextInput, Dimensions  } from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 
 interface TextFieldProps {
-    placeholder: string,
-    isSecure?: boolean,
-    onTextChange: Function,
+  placeholder: string;
+  isSecure?: boolean;
+  onTextChange: Function;
 }
 
-const TextField: React.FC<TextFieldProps> = ({ placeholder, isSecure = false, onTextChange }) => {
-
-    return(
-        <View style={styles.container} >
-            <TextInput 
-            placeholder={placeholder}
-            placeholderTextColor="white"
-            autoCapitalize='none'
-            secureTextEntry={isSecure}
-            onChangeText={(text) => onTextChange(text)}
-            style={styles.textField} />
-        </View>
-    )
-}
+const TextField: React.FC<TextFieldProps> = ({
+  placeholder,
+  isSecure = false,
+  onTextChange,
+}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor="white"
+        autoCapitalize="none"
+        secureTextEntry={isSecure}
+        onChangeText={text => onTextChange(text)}
+        style={styles.textField}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        height: 55,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 10,
-        marginLeft: 25,
-        marginRight: 25,
-        paddingRight: 10,
-        paddingLeft: 20,
-        borderWidth: 2,
-        borderColor: "white"
-    },
-    textField: {
-        flex: 1,
-        width: 320,
-        height: 50,
-        fontSize: 16,
-        color: "white"
-    }
-})
+  container: {
+    height: 55,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    marginLeft: 25,
+    marginRight: 25,
+    paddingRight: 10,
+    paddingLeft: 20,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  textField: {
+    flex: 1,
+    width: 320,
+    height: 50,
+    fontSize: 16,
+    color: 'white',
+  },
+});
 
-export { TextField }
+export {TextField};
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export * from './TextField';
+export * from './ButtonWithIcon';
+export * from './QuoteCard';
+export * from './CommentCard';
+export * from './CommentInput';
